998ee89f8cb5974affac3d038535cbeffeee328a
日直 敏宏 <x14g019@chiba-fjb.ac.jp>
2016-10-04 11:52:09 +0900

    SQL実装

--- app/src/main/java/jp/ac/chiba_fjb/x14b_c/naroreader/Bookmark.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_c/naroreader/Bookmark.java
@@ -1,6 +1,8 @@
 package jp.ac.chiba_fjb.x14b_c.naroreader;
 
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -19,6 +21,29 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
+
+//データベース管理用クラス
+class TestDB extends SQLite
+{
+
+    public TestDB(Context context) {
+        //ここでデータベースのファイル名とバージョン番号を指定
+        super(context, "test.db",1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //初期テーブルの作成
+        db.execSQL("create table test(name text);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //バージョン番号を変えた場合に呼び出される
+    }
+
+}
+
 public class Bookmark extends Fragment implements View.OnClickListener {
 
 
@@ -50,8 +75,10 @@ public class Bookmark extends Fragment implements View.OnClickListener {
         //インスタンスの取得
         LinearLayout layout = (LinearLayout) view.findViewById(R.id.BookmarkLayout);
         //動的なコントロールの生成
+        int MaxBookmark;
+        MaxBookmark = db.execSQL("select count() from DB");             //ブックマーク数を入れたい
         //TextViewを100個
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < MaxBookmark; i++) {
             TextView textView = new TextView(getContext());             //インスタンスの生成(引数はActivityのインスタンス)
             textView.setId(i);                     //テキストの設定
             layout.addView(textView);
