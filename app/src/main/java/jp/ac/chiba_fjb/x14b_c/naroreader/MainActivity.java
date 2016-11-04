f3918c62b949962307a43af741ea40b6261ed9ba
cloche <x14g006@chiba-fjb.ac.jp>
2016-11-04 21:11:15 +0900

    あ

--- app/src/main/java/jp/ac/chiba_fjb/x14b_c/naroreader/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_c/naroreader/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
 
