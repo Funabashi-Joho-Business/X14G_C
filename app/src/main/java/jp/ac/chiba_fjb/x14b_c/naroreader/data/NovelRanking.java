4c0d9fb4a9eb365ed569cf09b1a0916078b3a053
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2016-11-14 12:02:50 +0900

    ランキング取得部分の追加

---
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_c/naroreader/data/NovelRanking.java
@@ -0,0 +1,19 @@
+package jp.ac.chiba_fjb.x14b_c.naroreader.data;
+
+import java.util.Date;
+
+/**
+ * Created by oikawa on 2016/11/14.
+ */
+public class NovelRanking {
+    public String ncode;
+    public String title;
+    public String writerName;
+    public int writerId;
+    public Date update;
+    public String info;
+    public int novelCount;
+    public int point;
+    public int textCount;
+    public String genle;
+}
