5500322b30fe577eb0a1b08c5a4d2c15bdd768ac
cloche <x14g006@chiba->
2016-12-06 12:12:20 +0900

    Rankpointダイアログのボタンを見た目だけ有効化

--- app/src/main/java/jp/ac/chiba_fjb/x14b_c/naroreader/RankPointFragment.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_c/naroreader/RankPointFragment.java
@@ -8,12 +8,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RankPointFragment extends DialogFragment {
+public class RankPointFragment extends DialogFragment implements View.OnClickListener {
 
 
     public RankPointFragment() {
@@ -25,7 +27,16 @@ public class RankPointFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank_point, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_rank_point, container, false);
+
+        Button b1 = (Button) view.findViewById(R.id.button1);
+        Button b2 = (Button) view.findViewById(R.id.button2);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+
+        return view;
     }
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -34,4 +45,16 @@ public class RankPointFragment extends DialogFragment {
         return dialog;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button2:
+                //評価ポイントをつける処理
+                getDialog().cancel();
+                break;
+            case R.id.button1:
+                getDialog().cancel();
+                break;
+        }
+    }
 }
